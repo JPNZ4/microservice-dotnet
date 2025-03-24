8b4c22cb202bfc3469883e939d47d6c79de1f2b2
Jeremy Parker <jeremy.parker@emiratesteamnz.com>
2025-03-24 15:17:31 +1300

    fix controller

--- src/SearchService/Controllers/SearchController.cs
+++ src/SearchService/Controllers/SearchController.cs
@@ -11,37 +11,41 @@ namespace SearchService.Controllers;
 public class SearchController : ControllerBase
 {
     [HttpGet]
-    public async Task<ActionResult<List<Item>>> SearchItems([FromQuery]SearchParams searchParams)
+    public async Task<ActionResult<List<Item>>> SearchItems([FromQuery] SearchParams searchParams)
     {
         var query = DB.PagedSearch<Item, Item>();
 
-        query.Sort(x => x.Ascending(a => a.Make));
-
-        if (!string.IsNullOrEmpty(searchParams.SearchTerm)) 
+        if (!string.IsNullOrEmpty(searchParams.SearchTerm))
         {
             query.Match(Search.Full, searchParams.SearchTerm).SortByTextScore();
         }
 
         query = searchParams.OrderBy switch
         {
-            "make" => query.Sort(x => x.Ascending(a => a.Make)),
-            "new" => query.Sort(x => x.Descending(a => a.CreatedAt)),
-            _ => query.Sort(x => x.Ascending(a => a.AuctionEnd)),
+            "make" => query.Sort(x => x.Ascending(x => x.Make))
+                .Sort(x => x.Ascending(a => a.Model)),
+            "new" => query.Sort(x => x.Descending(x => x.CreatedAt)),
+            _ => query.Sort(x => x.Ascending(x => x.AuctionEnd))
         };
 
-        query = searchParams.FilterBy switch
+        if (!string.IsNullOrEmpty(searchParams.FilterBy))
         {
-            "finished" => query.Match(x => x.AuctionEnd < DateTime.UtcNow),
-            "endingSoon" => query.Match(x => x.AuctionEnd < DateTime.UtcNow.AddHours(6) && x.AuctionEnd < DateTime.UtcNow),
-            _ => query.Match(x => x.AuctionEnd > DateTime.UtcNow)
-        };
+            query = searchParams.FilterBy switch
+            {
+                "finished" => query.Match(x => x.AuctionEnd < DateTime.UtcNow),
+                "endingSoon" => query.Match(x =>
+                    x.AuctionEnd < DateTime.UtcNow.AddHours(6)
+                        && x.AuctionEnd > DateTime.UtcNow),
+                _ => query.Match(x => x.AuctionEnd > DateTime.UtcNow) // live
+            };
+        }
 
-        if (!string.IsNullOrEmpty(searchParams.Seller)) 
+        if (!string.IsNullOrEmpty(searchParams.Seller))
         {
             query.Match(x => x.Seller == searchParams.Seller);
         }
 
-         if (!string.IsNullOrEmpty(searchParams.Winner)) 
+        if (!string.IsNullOrEmpty(searchParams.Winner))
         {
             query.Match(x => x.Winner == searchParams.Winner);
         }
@@ -51,11 +55,11 @@ public class SearchController : ControllerBase
 
         var result = await query.ExecuteAsync();
 
-        return Ok(new {
+        return Ok(new
+        {
             results = result.Results,
             pageCount = result.PageCount,
             totalCount = result.TotalCount
-
         });
     }
 }
