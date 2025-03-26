b2749f78566638ee0d09337ea8475933c9ae8a0a
Jeremy Parker <jeremy.parker@emiratesteamnz.com>
2025-03-27 09:32:19 +1300

    add mass transit and contracts sln

--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -1,14 +1,23 @@
 using AuctionService.Data;
+using MassTransit;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
-builder.Services.AddDbContext<AuctionDbContext>(opt => {
+builder.Services.AddDbContext<AuctionDbContext>(opt =>
+{
     opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddMassTransit(x =>
+{
+    x.UsingRabbitMq((context, cfg) =>
+    {
+        cfg.ConfigureEndpoints(context);
+    });
+});
 
 var app = builder.Build();
 
@@ -18,10 +27,10 @@ app.UseHttpsRedirection();
 
 app.MapControllers();
 
-try 
+try
 {
     DbInitializer.InitDb(app);
-} 
+}
 catch (Exception ex)
 {
     Console.WriteLine(ex.Message);
