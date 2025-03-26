e93764a0990aba3b33dca43a45fe4f545deedf7a
Jeremy Parker <jeremy.parker@emiratesteamnz.com>
2025-03-26 19:18:14 +1300

    use http service to get data from auctions into search

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -2,15 +2,14 @@ using MongoDB.Driver;
 using MongoDB.Entities;
 using SearchService.Data;
 using SearchService.Models;
+using SearchService.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-
+builder.Services.AddHttpClient<AuctionSvcHttpClient>();
 var app = builder.Build();
 
 app.UseAuthorization();
